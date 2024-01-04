91d6c69d4d583808f21889a00caa50f2c240fbbb
niklasadams <niklas.adams@pads.rwth-aachen.de>
2024-01-04 08:38:31 +0100

    fix call by reference/value bug in the noise generation method

--- ocpa/algo/util/noise/events.py
+++ ocpa/algo/util/noise/events.py
@@ -15,27 +15,28 @@ def switch_activities(ocel, noise_level, allowed_switches):
             #permutation of activity pairs
             activity_pairs_perm = random.sample(activity_pairs, len(activity_pairs))
             for activity_pair in activity_pairs_perm:
-                if row['event_activity'] == activity_pair[0] and pd.notna(row[object_type]):
-                    object_list = row[object_type]
-                    if len(object_list) > 0 and random.random()<noise_level:
-                        removed_object = random.choice(object_list)
-                        object_list.remove(removed_object)
+                if not applied_noise:
+                    if row['event_activity'] == activity_pair[0] and pd.notna(row[object_type]):
+                        row_object_list = row[object_type]
+                        if len(row_object_list) > 0 and random.random()<noise_level:
+                            object_list = [o for o in row_object_list]
+                            removed_object = random.choice(object_list)
+                            object_list = [o for o in object_list if o != removed_object]
 
-                        new_event_df.at[index,object_type] = object_list
-                        new_row = row.copy()
-                        new_row['event_activity'] = activity_pair[1]
-                        new_row['event_timestamp'] = new_row['event_timestamp']+ pd.to_timedelta(1, unit='s')
-                        new_row[object_type] = [removed_object]
-                        for ot in ocel.object_types:
-                            if ot!= object_type:
-                                new_row[ot] = []
+                            new_event_df.at[index,object_type] = object_list
+                            new_row = row.copy()
+                            new_row['event_activity'] = activity_pair[1]
+                            new_row['event_timestamp'] = new_row['event_timestamp']+ pd.to_timedelta(1, unit='s')
+                            new_row[object_type] = [removed_object]
+                            for ot in ocel.object_types:
+                                if ot!= object_type:
+                                    new_row[ot] = []
 
-                        # Add the new row to the list
-                        new_rows.append(new_row)
-                        applied_noise = True
-                        break
-            if applied_noise:
-                break
+                            # Add the new row to the list
+                            new_rows.append(new_row)
+                            applied_noise = True
+
+
     # Add new rows to the original DataFrame
     for new_row in new_rows:
         new_event_df = new_event_df.append(new_row, ignore_index=True)
